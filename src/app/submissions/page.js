23d1e636719df07778f5f769c47b3b2a94a8609f
Sheraz Ali <sa0071429@gmail.com>
2025-10-14 10:59:52 -0700

    Update page.js

--- src/app/submissions/page.js
+++ src/app/submissions/page.js
@@ -12,7 +12,7 @@ export default function SubmissionsPage() {
   useEffect(() => {
     const fetchSubmissions = async () => {
       try {
-        const response = await fetch('/ap/submissions');
+        const response = await fetch('/api/v1/submissions');
         const result = await response.json();
         
         if (result.success) {
